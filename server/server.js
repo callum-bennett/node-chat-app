88067c4c8ef04a7d338b98d31080ed97fb4dea4a
Callum Bennett <callum.bennett@enovation.ie>
2017-08-28 21:26:47 -0500

    Add newMessage and create message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Server',
+    text: 'This is a message from the server',
+  });
+
+  socket.on('createMessage', message => {
+    console.log('Create message', message);
+  });
+
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
